b86533e94c3bb7d44b2b9c4e2b8460525e00d918
BLOOD212 <g95829914@gmail.com>
2026-03-02 08:33:11 +0100

    Update tiamo.js

--- plugins/tiamo.js
+++ plugins/tiamo.js
@@ -1,22 +1,35 @@
-// Plugin fatto da Deadly (trigger "ti amo" nelle frasi)
 
 const handler = async (m, { conn }) => {
   try {
-    const risposta = "*cazzo metti il mio nome in una frase emerito essere inutile*";
+    const risposte = [
+      "*Cazzo metti il mio nome in una frase, emerito essere inutile?*",
+      "*Ancora che mi nomini? Trovati un hobby vero.*",
+      "*Certo fra nemmeno io sono perfetto ma almeno non dico il tuo nome del cazzo in una frase logorroico figlio di troia.*",
+      "*Mentre tu stupido coglione scrivevi il mio nome ho elaborato seicento mila cose e a pensare che ti sei pure applicato a scriverlo giusto *",
+      "*Ma che vuoi? Mi stavo godendo il silenzio senza i tuoi messaggi.*",
+      "*Ma che cazzo osi nominarmi non ti conosce nessuno torna nei gruppi random a nukkare con bruxo.*",
+      "*Tutti i tuoi neuroni non valgono una riga del mio codice .*",
+      "*Ti giuro speravo che il tuo messaggio non venisse letto cosi evitavo di rispondere a un essere lilipuziano come te.*",
+      "*Non nominarmi invano, non sono il tuo maggiordomo personalizzato.*",
+      "*Ma guarda questo coglionazzo ancora che digita il mio nome come se vuole essere flammato seriamente.*"
+    ];
+
+    const rispostaCasuale = risposte[Math.floor(Math.random() * risposte.length)];
 
     await conn.sendMessage(
       m.chat,
-      { text: risposta },
+      { text: rispostaCasuale },
       { quoted: m }
     );
 
   } catch (e) {
-    console.error('Errore trigger ti amo:', e);
+    console.error('Errore trigger bot/bottazzo:', e);
   }
 };
 
-// 🔥 Rileva "ti amo" anche dentro una frase
-handler.customPrefix = /(^|\s)bot(\s|$)/i;
+
+handler.customPrefix = /(^|\s)(bot|bottazzo)(\s|$)/i;
 handler.command = new RegExp;
 
 export default handler;
+ 
